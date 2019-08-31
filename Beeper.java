e816b79419a020e3403abbc1da7bad58dbf994ed
thepunywolfie <anirudhragavender@gmail.com>
2019-08-31 14:34:08 +0530

    Add files via upload

---
+++ Beeper.java
@@ -0,0 +1,39 @@
+package com.example.firebasedemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.Toast;
+
+public class Beeper extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activiy_beeper);
+
+        final Button customButton = findViewById(R.id.custom_button);
+        Switch switchEnableButton = findViewById(R.id.switch_enable_button);
+
+        customButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(Beeper.this, "Click", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        switchEnableButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    customButton.setEnabled(true);
+                } else {
+                    customButton.setEnabled(false);
+                }
+            }
+        });
+    }
+}
